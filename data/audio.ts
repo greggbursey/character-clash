3f57f78fbd049ceeafb997ccca46771c5c5d2c1c
Gregg Bursey <greggburseyjr@gmail.com>
2026-04-03 17:20:31 -0500

    feat: add Before_the_Iron_Gate track to BATTLE_TRACKS and include associated audio file

--- data/audio.ts
+++ data/audio.ts
@@ -2,4 +2,5 @@ export const BATTLE_TRACKS = [
   "/audio/epic-battle-music.mp3",
   "/audio/ominous-battle-music.mp3",
   "/audio/positive-battle-music.mp3",
+  "/audio/Before_the_Iron_Gate.mp3",
 ];
